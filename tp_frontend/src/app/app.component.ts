b601d1da2096c98bf21fd6734a5b9236e1bd5ec3
znttg <zanattagiovanni@gmail.com>
2024-07-15 16:29:07 -0400

    Implementation of basic routing

--- tp_frontend/src/app/app.component.ts
+++ tp_frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
--- tp_frontend/src/app/app.routes.ts
+++ tp_frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'signup', component: SignupComponent},
+    { path: 'login', component: LoginComponent }
+];
