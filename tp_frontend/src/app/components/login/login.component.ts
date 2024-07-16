10dfa94efd2ab805637274c01d1c4ebc24a9a9a0
znttg <zanattagiovanni@gmail.com>
2024-07-15 21:22:44 -0400

    Component position change

--- tp_frontend/src/app/components/login/login.component.ts
+++ tp_frontend/src/app/components/login/login.component.ts
@@ -4,7 +4,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- tp_frontend/src/app/components/signup/signup.component.ts
+++ tp_frontend/src/app/components/signup/signup.component.ts
@@ -4,7 +4,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
