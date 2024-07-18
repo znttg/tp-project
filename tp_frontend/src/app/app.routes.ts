57111a4746677a62b78d13087795fd7b60f193ca
znttg <zanattagiovanni@gmail.com>
2024-07-18 14:12:33 -0400

    Get clients complete

--- tp_frontend/src/app/app.routes.ts
+++ tp_frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ClientsComponent } from './components/clients/clients.component';
-import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
